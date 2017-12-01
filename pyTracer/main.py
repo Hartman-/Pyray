8062052845b9bb7fa66ea72155e4412c6f320a89
Hartman- <thehartman8@gmail.com>
2017-11-30 22:38:53 -0500

    updates to scatter function, reversed env light to get contact shadows

--- pyTracer/main.py
+++ pyTracer/main.py
@@ -12,7 +12,6 @@ from camera import camera
 from imageo import output_image
 
 rgb = vec3
-sys.setrecursionlimit(1500)
 
 
 # Class to store hit data
@@ -21,12 +20,16 @@ class hit_record(object):
 
 
 def random_in_unit_sphere(shape):
-    p = vec3(0.0, 0.0, 0.0)
-    # while True:
-        # p = (vec3(np.random.rand(shape), np.random.rand(shape), np.random.rand(shape))*2.0) - vec3(1.0, 1.0, 1.0)
-        # if (p.squared_length() < 1.0 and p.squared_length() > 0.001):
-            # break
-    p = (vec3(np.random.rand(shape), np.random.rand(shape), np.random.rand(shape)).norm()*2.0) - vec3(1.0, 1.0, 1.0)
+    p = (vec3(np.random.rand(shape), np.random.rand(shape), np.random.rand(shape))*2.0) - vec3(1.0, 1.0, 1.0)
+
+    while True:
+        cond = (p.squared_length() >= 1.0)
+        rp = (vec3(np.random.rand(shape), np.random.rand(shape), np.random.rand(shape))*2.0) - vec3(1.0, 1.0, 1.0)
+        np.place(p.x, cond, rp.x)
+        np.place(p.y, cond, rp.y)
+        np.place(p.z, cond, rp.z)
+        if not np.any(cond):
+            break
     return p
 
 
@@ -42,11 +45,11 @@ def raytrace(r, scene):
     unit_dir = unit_vector(r.direction())
     t = (unit_dir.y + 1.0)*0.5
     bgc = (vec3(1.0, 1.0, 1.0)*(1.0 - t) + vec3(0.5, 0.7, 1.0)*t)
-
+    bgc = vec3(0.5, 0.7, 1.0) * t
     for (s, d) in zip(scene, distances):
         hit = (nearest != 1.0e39) & (d == nearest)
-
-        print(hit.shape)
+        # print(s.radius)
+        # print(hit.shape)
         if np.any(hit):
             dc = np.extract(hit, d)
             oc = r.origin().extract(hit)
@@ -57,7 +60,10 @@ def raytrace(r, scene):
             pe = er.point_at_parameter(dc)
 
             N = (pe - s.center) / vec3(s.radius, s.radius, s.radius)
-            target = pe + N + random_in_unit_sphere(dc.shape[0])
+
+            randunit = random_in_unit_sphere(dc.shape[0])
+            target = pe + N
+            # print(target.components())
             # output_image(target-pe, 400, 200)
             # tc = (vec3(target.x + 1, target.y+1, target.z+1) * 0.5)
             # color += Nc * (nearest != 1.0e39) * (d == nearest)
@@ -72,11 +78,11 @@ def raytrace(r, scene):
 def main():
     t0 = time.time()
 
-    nx = 400
-    ny = 200
+    nx = 600
+    ny = 300
 
     world = [Sphere(vec3(0, 0, -1), 0.5), Sphere(vec3(0, -100.5, -1), 100)]
-
+    # world = [Sphere(vec3(0, -100.5, -1), 100)]
     # Build array of vectors defined on a normalized plane
     # aspect ratio
     # ratio = float(nx) / float(ny)
